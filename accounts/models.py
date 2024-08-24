e3c1c8f5e2eee746dc4609dd9f33d8d8e4cd6479
fukushigetomoya <t.fukushige.24.3.1302@4nonome.com>
2024-08-24 09:16:59 +0900

    カスタムユーザーモデルの実装

--- accounts/models.py
+++ accounts/models.py
@@ -6,3 +6,10 @@
 
 
 # class FriendShip(models.Model):
+
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    email = models.EmailField()
