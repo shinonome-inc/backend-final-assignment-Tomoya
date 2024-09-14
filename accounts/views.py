ab58bc2963a2bfbb38d03fa70b67a6f846b9d970
fukushigetomoya <t.fukushige.24.3.1302@4nonome.com>
2024-09-14 13:16:40 +0900

    プロフィール画面の実装

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,12 @@
 from django.conf import settings
 from django.contrib.auth import authenticate, login
+from django.shortcuts import get_object_or_404
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
+from django.views.generic.detail import DetailView
 
 from .forms import SignupForm
+from .models import User
 
 
 class SignupView(CreateView):
@@ -18,3 +21,13 @@ class SignupView(CreateView):
         user = authenticate(self.request, username=username, password=password)
         login(self.request, user)
         return response
+
+
+class UserProfileView(DetailView):
+    model = User
+    template_name = 'accounts/user_profile.html'
+    context_object_name = 'user'
+
+    def get_object(self, queryset=None):
+        username = self.kwargs.get('username')
+        return get_object_or_404(User, username=username)
