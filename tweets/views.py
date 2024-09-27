341a10488d80a586883625b058b01906970c29f3
fukushigetomoya <t.fukushige.24.3.1302@4nonome.com>
2024-09-27 13:07:34 +0900

    Tweet作成・表示機能の追加

--- tweets/views.py
+++ tweets/views.py
@@ -1,7 +1,25 @@
 # from django.shortcuts import render
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic.base import TemplateView
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, ListView
+
+from .forms import TweetForm
+from .models import Tweet
 
 
-class HomeView(LoginRequiredMixin, TemplateView):
-    template_name = "tweets/home.html"
+class HomeView(LoginRequiredMixin, ListView):
+    model = Tweet
+    template_name = 'tweets/home.html'
+    context_object_name = 'tweets'
+    ordering = ['-created_at']
+
+
+class TweetCreateView(CreateView):
+    model = Tweet
+    form_class = TweetForm
+    template_name = 'tweets/tweet_form.html'
+    success_url = reverse_lazy('tweets:home')
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
